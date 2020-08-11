2799d174afdd4b3e5db8fbed9e4dfbaaf6b6e365
SWFullPotential <swfullpotential@gmail.com>
2020-08-11 12:49:20 -0600

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resource :products 
-  resource :application
+  # resource :products 
+  # resource :application
 
   root 'products#index'
   post '/' => 'products#add'
