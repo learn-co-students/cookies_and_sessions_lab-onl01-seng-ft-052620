24c677b8d80b7793a21cc84ad49f8ddc7a1da2a4
francepants <franceline90@gmail.com>
2020-08-11 11:20:16 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,14 @@
 class ApplicationController < ActionController::Base
+  #add helper method :cart here to use cart in prod. controller
+  helper_method :cart
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  #1. ApplicationController cart returns the user's shopping cart, an initially empty array
+  #2. creates a cart in the session if one doesn't exist
+  #3. returns the existing cart if one already exists
+  def cart
+    session[:cart] ||= []
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  resource :application
+  resource :products
+  
+  # get 'products/index'
+
+  # get 'products/add'
+
+  root 'products#index'
+  post '/' => 'products#add'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
